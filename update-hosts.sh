220900d6bc451fe7431bd3552453f8374c599922
Nathan Graham <ngraham@kumc.edu>
2016-09-07 17:56:56 -0500

    Credit where due for awk magic.
     - Automate hosts file fix-up (UNTESTED).

--- update-hosts.sh
+++ update-hosts.sh
@@ -1,1 +1,2 @@
-awk '$1~"^127.0.0.1|::1"{$2="'`hostname -s`'\ '`hostname`' "$2}1' OFS="\t" /etc/hosts > /etc/hosts
+# ACK: https://www.krenger.ch/blog/fatal-prvf-0002-could-not-retrieve-local-nodename/
+awk '$1~"^127.0.0.1|::1"{$2="'`hostname -s`'\ '`hostname`' "$2}1' OFS="\t" /etc/hosts > /tmp/hosts
