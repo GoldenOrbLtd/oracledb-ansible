72ecfbe8d338c61553cf155fd9a0563691a8e37f
Nathan Graham <ngraham@kumc.edu>
2016-09-09 16:59:45 -0500

    Separate Oracle data files from installation.
     - Use "DATA_WAREHOUSE" template for database creation
     - Don't install "sample" data
     - Set Oracle environment variables in profile.d - use template rather than hard-coded values.
     - Use "become" to avoid Ansible deprecation errors
     - Remove Vagrant-specific steps

--- oraenv.sh
+++
@@ -1,4 +0,0 @@
-export ORACLE_HOME=/oracle/app/oracle/product/12102/dbhome_1
-export PATH=$PATH:$ORACLE_HOME/bin
-export LD_LIBRARY_PATH=$ORACLE_HOME/lib
-export TNS_ADMIN=$ORACLE_HOME/network/admin
